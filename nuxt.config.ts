53a6d8ccb1d92f0dfe0cf2ca36d24ae3ceaa0591
santedev <santiagocortes132@gmail.com>
2024-07-22 23:31:26 -0500

    new new deploy config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  app: {
-    baseURL: process.env.NUXT_APP_BASE_URL || '/'
-  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
